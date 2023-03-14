f8cb36078098967fac59b1379b4d4be58bbc2083
Bluju <JASahagun26@gmail.com>
2023-03-14 14:08:15 -0500

    Finished the Weird Algorithm problem

--- P1/WeirdAlgorithm.cpp
+++ P1/WeirdAlgorithm.cpp
@@ -1,1 +1,20 @@
+//Consider an algorithm that takes as input a positive integer n. If n is even, the algorithm divides by two,
+//if n is odd, the algorithm multiplies by three and adds one.
+//Repeat until n is one
+#include <bits/stdc++.h>
 
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    long long n;
+    cin >> n;
+    cout << n << " ";
+    while(n > 1){
+        n = n % 2 == 0 ? n/2 : n*3+1;
+        cout << n << " ";
+    }
+    cout << "\n";
+}
