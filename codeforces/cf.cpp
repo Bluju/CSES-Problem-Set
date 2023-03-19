b48956c358cd7f17799454d3340bab9d04225b3e
Bluju <JASahagun26@gmail.com>
2023-03-19 11:48:14 -0500

    Finished 2 problems for Codeforces Round 859 (Div. 4)

--- codeforces/cf.cpp
+++ codeforces/cf.cpp
@@ -1,11 +1,25 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
 int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
 
+    int testCases;
+    cin >> testCases;
+
+    int a, b, c;
+
+    for(int i = 0; i < testCases;i++){
+        //loop through the test cases and output whether + or - make the equation true
+        //a + b = c
+        cin >> a >> b >> c;
+        if(a + b == c){
+            printf("+\n");
+        }
+        else if(a - b == c){
+            printf("-\n");
+        }
+    }
 
 
 }
