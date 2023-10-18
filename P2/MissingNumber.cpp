8a54a08dedb9eb71541ce0eb45734374bd1c738a
Julian <43455717+Bluju@users.noreply.github.com>
2023-10-18 13:41:49 -0500

    Update MissingNumber.cpp

--- P2/MissingNumber.cpp
+++ P2/MissingNumber.cpp
@@ -3,7 +3,27 @@
 using namespace std;
 
 int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
+    int n;
+    cin >> n;
+    int arr[n-1];
+    for(int i = 0; i < n-1; i++){
+        cin >> arr[i];
+    }
+    //sort the array
+    sort(arr , arr+n-1);
+    //find the missing number
+    int missingNum = 1;
+    bool found = false;
+    for(int i = 0; i < n - 1; i++){
+        if(missingNum != arr[i]){
+            printf("%d\n", missingNum);
+            found = true;
+            break;
+        }
+        ++missingNum;
+    }
+    if(!found){
+        missingNum = n;
+        printf("%d\n", missingNum);
+    }
 }
