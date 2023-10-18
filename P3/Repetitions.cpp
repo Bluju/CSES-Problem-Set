0533dcf9084aaee656efd7a5d3bbcd2878bf4e66
Julian <43455717+Bluju@users.noreply.github.com>
2023-10-18 14:27:20 -0500

    Create Repetitions.cpp

---
+++ P3/Repetitions.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+  //Given DNA sequence: string consisting of chars A,C,G,T
+  //Find the longest repetition in the sequence
+int main(){
+  string input;
+
+  cin >> input;
+
+  char currChar;
+  int longest = 0;
+  for(int i = 0; i < input.length();i++){
+
+  }
+
+}
